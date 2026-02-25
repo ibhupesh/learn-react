3d29804438ae1f12d9bc4857230f39e612a0eba5
ibhupesh <ibhupesh@outlook.com>
2026-02-25 23:54:13 +0530

    JSX basics

--- learn-react/src/App.js
+++ learn-react/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
+let name = "React";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React in a cool way!
-        </a>
-      </header>
+    <>
+    <h1> Welcome to {name} tutorials</h1>
+    <nav>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contact</li>
+    </nav>
+    <div className ="content">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat blanditiis eaque quas nam esse commodi tempore officiis, voluptas est, iste consequatur facere animi necessitatibus totam. Officiis similique nam quia consequatur?</p>
     </div>
+    </>
   );
 }
 
